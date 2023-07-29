1e47b9145c9d11cd61a7edff12a762344daffd21
84h3p <43922329+84h3p@users.noreply.github.com>
2023-07-29 10:46:39 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,6 +1,6 @@
-carList = {'bullet', 'infernus', 'emperor', 'dominator', 'tornado', 'buffalo', 'penumbra'}
-streetList = {vector3(317.23, -206.43, 54.08), vector3(272.81, 66.97, 99.89), vector3(383.62, -767.88, 29.29), vector3(-255.17, -2655.86, 5.99)}
-destinationList = {vector3(367.92, 335.42, 102.81), vector3(-10.26, -1082.36, 26.67), vector3(890.13, -1591.53, 30.19), vector3(1189.12, -1322.62, 34.97)}
+local carList = {'bullet', 'infernus', 'emperor', 'dominator', 'tornado', 'buffalo', 'penumbra'}
+local streetList = {vector3(317.23, -206.43, 54.08)}
+local destinationList = {vector3(367.92, 335.42, 102.81), vector3(-10.26, -1082.36, 26.67), vector3(890.13, -1591.53, 30.19), vector3(1189.12, -1322.62, 34.97)}
 
 
 RegisterCommand('carjack', function(source, args)
@@ -10,10 +10,10 @@ RegisterCommand('carjack', function(source, args)
     RemoveBlip(destinationBlip)
 
     -- random car generation
-    vehicleName = carList[math.random(#carList)]
+    local vehicleName = carList[math.random(#carList)]
 
     -- random street generation
-    streetName = streetList[math.random(#streetList)]
+    local streetName = streetList[math.random(#streetList)]
 
     RequestModel(vehicleName)
 
@@ -23,40 +23,59 @@ RegisterCommand('carjack', function(source, args)
     end
 
     -- create the vehicle
-    vehicle = CreateVehicle(vehicleName, streetName, true, false)
-    SetVehicleDoorsLocked (vehicle, 2)
+    local vehicle = CreateVehicle(vehicleName, streetName, true, true)
+    local netID = VehToNet(vehicle)
+    local vehicleID = NetToVeh(netID)
+    SetNetworkIdCanMigrate(id, true)
+    SetEntityAsMissionEntity(vehicle, true, true)
+    SetVehicleHasBeenOwnedByPlayer(vehicle, true)
+    SetEntitySomething(vehicle, true)
+    SetNetworkIdExistsOnAllMachines(netID, true)
+    SetVehicleDoorsLocked (netID, 2)
+    print(vehicleID)
+    print(netID)
+    lockpicked = false
+    
+
+    
     local plates = GetVehicleNumberPlateText(vehicle)
     
-    car = AddBlipForCoord(streetName)
+    local car = AddBlipForCoord(streetName)
 
     TriggerEvent('chat:addMessage', {
 		args = { 'Тебе нужно угнать ' .. vehicleName .. '. Привези мне его. Мне она нужна целой. Координаты скинул.' .. ' Номер: ' .. plates .. '.'}
 	})
 
-
-    while GetVehiclePedIsIn(GetPlayerPed(-1), False) ~= vehicle do
+    while lockpicked == false do
         Citizen.Wait(0)
-        distance = GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), streetName, false)
-        
+            
+        local distance = GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), streetName, false)
+
         if IsControlJustReleased(0, 206) and (distance < 5) then
             SetDisplay(not display) 
         end
-
-        if GetVehiclePedIsIn(GetPlayerPed(-1), False) == vehicle then
-            RemoveBlip(car)
-            TriggerEvent('chat:addMessage', {
-                args = { 'Скидываю координаты' }
-            })
-            destinationName = destinationList[math.random(#destinationList)]
-            destinationBlip = AddBlipForCoord(destinationName)
-        end
     end
+
+    SetVehicleDoorsLocked (vehicleID, 1)
+    SetVehicleAlarm(vehicleID, true)
+    StartVehicleAlarm(vehicleID)
+
+    while GetVehiclePedIsIn(GetPlayerPed(-1), False) ~= vehicleID do
+        Citizen.Wait(0)
+    end
+
+    RemoveBlip(car)
+    TriggerEvent('chat:addMessage', {
+        args = { 'Скидываю координаты' }
+    })
+    destinationName = destinationList[math.random(#destinationList)]
+    destinationBlip = AddBlipForCoord(destinationName)
 
 
     while GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), destinationName, false) > 5 do
         Citizen.Wait(1000)
 
-        vehiclehealth = GetVehicleBodyHealth(vehicle)
+        vehiclehealth = GetVehicleBodyHealth(vehicleID)
 
         if vehiclehealth < 900 then 
             TriggerEvent('chat:addMessage', {
@@ -67,18 +86,16 @@ RegisterCommand('carjack', function(source, args)
 
             goto after_lose
         end
-        
-        if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), destinationName, false) < 5 then
-            TriggerEvent('chat:addMessage', {
-                args = { 'Круто, сдал тачку' }
-            })
-            RemoveBlip(destinationBlip)
-        end 
     end
+
+    TriggerEvent('chat:addMessage', {
+        args = { 'Круто, сдал тачку' }
+    })
+    RemoveBlip(destinationBlip)
 
     ::after_lose::
         -- give the vehicle back to the game (this'll make the game decide when to despawn the vehicle)
-    SetEntityAsNoLongerNeeded(vehicle)
+    SetEntityAsNoLongerNeeded(vehicleID)
 
         -- release the model
     SetModelAsNoLongerNeeded(vehicleName)
